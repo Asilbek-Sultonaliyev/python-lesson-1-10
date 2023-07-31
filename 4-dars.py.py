590221323f6b997672e64faebbeb43aa935cbe20
Asilbek Sultonaliyev <avazbeksultonaliyev16@gmail.com>
2023-07-31 15:20:39 +0500

    Yangi darslar qo'shildi
    3-4-5-darslar yuklandi

---
+++ 4-dars.py.py
@@ -0,0 +1,38 @@
+# 4-dars. O'zgaruvchilar
+# sana:20/07/2023
+
+ism='Asilbek'
+yosh=14
+print(ism)
+print(yosh)
+
+ism='Asilbek'
+print(ism)
+ism="Muhammad"
+print(ism)
+
+ism_sharif='Asilbek Sultonaliyev'
+print(ism_sharif)
+
+a=4
+b=5
+c=(a+b)**3
+print(c)
+
+#ism,Ism,ISM so'zlari har xil talqin qilinadi
+#o'zgaruvchi harf yoki pastki chiziq bilan boshlanishi kerak
+#o'zgaruvchi raqam bilan boshlanmaydi
+
+                         # Amaliyot
+salom=("HELLO WORLD!")
+print(salom)
+
+xabar="kelaqol"
+print(xabar)
+xabar="tezroq bo'l"
+print(xabar)
+
+radius=5
+pi=3.14159
+aylana_yuzi=pi*radius**2
+print("Radiusi" ,radius, "ga teng aylananing yuzi=" ,aylana_yuzi)
